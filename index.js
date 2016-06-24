e7cd22e3a998fe4cdca8cf9cba55574879d18357
spat-ne-hochu <spat.ne.hochu@gmail.com>
2016-06-24 23:37:00 +0300

    variable & string feature

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
+let Node = require('@combinejs/node');
+
 class OutputDirective {
-    constructor(value) {
-        this._value = value;
+    constructor(value, node) {
+        if (value[0] === '"' && value[value.length - 1] === '"') {
+            let child = new Node('String');
+            child.string = value.slice(1, -1);
+        } else {
+            let child = new Node('String');
+            child.expression = value.split('.');
+        }
+
+        node.addChild(child);
     }
 }
 
